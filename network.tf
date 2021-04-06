fb0347244bae22395cdd8276457e2a3188f160f3
Neil L Zimmerman <nlz@nlz.me>
2021-04-05 21:00:54 -0400

    three subnets that hopefully route

--- network.tf
+++ network.tf
@@ -8,10 +8,20 @@ resource aws_vpc vpc {
 
 data aws_availability_zones available {}
 
+variable availability_zone_names {
+  type = list(string)
+  default = []
+}
+
+# If you don't specify availability zones, use all of them.
+locals {
+  availability_zones = length(var.availability_zone_names) == 0 ? data.aws_availability_zones.available.names : var.availability_zone_names
+}
+
 resource aws_subnet private_subnets {
   # https://www.terraform.io/docs/language/meta-arguments/count.html
   # https://www.terraform.io/docs/language/meta-arguments/for_each.html
-  count = length(data.aws_availability_zones.available.names)
+  count = length(local.availability_zones)
   cidr_block = cidrsubnet("10.250.0.0/17", ceil(log(length(data.aws_availability_zones.available.names), 2)), count.index)
   vpc_id = aws_vpc.vpc.id
   map_public_ip_on_launch = false
@@ -21,9 +31,93 @@ resource aws_subnet private_subnets {
 resource aws_subnet public_subnets {
   # https://www.terraform.io/docs/language/meta-arguments/count.html
   # https://www.terraform.io/docs/language/meta-arguments/for_each.html
-  count = length(data.aws_availability_zones.available.names)
+  count = length(local.availability_zones)
   cidr_block = cidrsubnet("10.250.128.0/17", ceil(log(length(data.aws_availability_zones.available.names), 2)), count.index)
   vpc_id = aws_vpc.vpc.id
   map_public_ip_on_launch = true
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone = local.availability_zones[count.index]
+}
+
+
+resource aws_internet_gateway igw {
+  vpc_id = aws_vpc.vpc.id
+}
+
+# https://dev.betterdoc.org/infrastructure/2020/02/04/setting-up-a-nat-gateway-on-aws-using-terraform.html
+resource aws_eip nat_gateways {
+  count = length(local.availability_zones)
+  vpc = true
+}
+
+resource aws_nat_gateway nat_gateways {
+  count = length(aws_subnet.private_subnets)
+  allocation_id = aws_eip.nat_gateways[count.index].id
+  subnet_id = aws_subnet.public_subnets[count.index].id
+}
+
+resource aws_route_table public_route_table {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource aws_route public_route {
+  route_table_id = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource aws_route_table_association public_route_association {
+  count = length(aws_subnet.public_subnets)
+  subnet_id = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource aws_route_table private_route_tables {
+  count = length(aws_subnet.private_subnets)
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource aws_route private_route {
+  count = length(aws_subnet.private_subnets)
+  route_table_id = aws_route_table.private_route_tables[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat_gateways[count.index].id
+}
+#
+resource aws_route_table_association private_route_associations {
+  count = length(aws_subnet.private_subnets)
+  subnet_id = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_tables[count.index].id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Let people SSH in"
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_security_group_rule" "ssh_in" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_ssh.id
+}
+
+resource "aws_security_group_rule" "allow_local" {
+  type = "ingress"
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  cidr_blocks = [aws_vpc.vpc.cidr_block]
+  security_group_id = aws_security_group.allow_ssh.id
+}
+
+resource "aws_security_group_rule" "allow_egress" {
+  type = "egress"
+  from_port = 0
+  to_port = 65535
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_ssh.id
 }
